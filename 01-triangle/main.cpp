889656f3ef173c9b2119d8ffc99a0a127a90039a
toughcactus <liuyixuan25@rocketmail.com>
2021-11-10 15:45:45 +0800

    delete extra code

--- 01-triangle/main.cpp
+++ 01-triangle/main.cpp
@@ -60,42 +60,6 @@ init(void)
     glEnableVertexAttribArray(vPosition);
 }
 
-#if 0
-void
-init( void )
-{
-    glGenVertexArrays( NumVAOs, VAOs );
-    glBindVertexArray( VAOs[Triangles] );
-    GLfloat  vertices[NumVertices][2] = {
-        { -0.90f, -0.90f }, {  0.85f, -0.90f }, { -0.90f,  0.85f },  // Triangle 1
-        {  0.90f, -0.85f }, {  0.90f,  0.90f }, { -0.85f,  0.90f }   // Triangle 2
-    };
-
-    //glCreateBuffers is API of openGL4.5
-    //refer to https://stackoverflow.com/questions/31841494/difference-in-glgenbuffers-and-glcreatebuffers
-    glGenBuffers( NumBuffers, Buffers );
-    
-    glBindBuffer( GL_ARRAY_BUFFER, Buffers[ArrayBuffer] );
-    
-    //glBufferStorage is API of openGL4.5
-    //glBufferStorage( GL_ARRAY_BUFFER, sizeof(vertices), vertices, 0);
-    glBufferData( GL_ARRAY_BUFFER, sizeof(vertices), vertices,  GL_STATIC_DRAW);
-    
-    ShaderInfo  shaders[] =
-    {
-        { GL_VERTEX_SHADER, "/Volumes/OpenGL/Projects/test/test/triangles.vert" },
-        { GL_FRAGMENT_SHADER, "/Volumes/OpenGL/Projects/test/test/triangles.frag" },
-        { GL_NONE, NULL }
-    };
-
-    GLuint program = LoadShaders( shaders );
-    glUseProgram( program );
-
-    glVertexAttribPointer( vPosition, 2, GL_FLOAT,
-                           GL_FALSE, 0, BUFFER_OFFSET(0) );
-    glEnableVertexAttribArray( vPosition );
-}
-#endif
 void
 display(void)
 {
